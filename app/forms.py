64ad91268d006ff673f7165440f0981432ac1f2f
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-12 17:32:21 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,5 +6,5 @@ class ContactForm(Form):
 name=StringField('Name',[validators.Length(min=5, max=10)])
 email=StringField('Email Address',[validators.Length(min=6,max=20)])
 subject=StringField('Subject',[validators.Length(min=5, max=10)])
-message =('Message',[validators.Length(min=20, max=40)])
+message =TextAreaField('Message',[validators.Length(min=20, max=40)])
 
