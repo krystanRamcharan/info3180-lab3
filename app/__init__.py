5815b9a1cfc784972dab0d143fbdec059fc7f409
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-12 17:02:57 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_mail import Mail
 
+csrf = CSRFProtect()
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hello friend'
@@ -10,4 +13,6 @@ app.config['MAIL_USERNAME'] = '7b8508f6d62ee5'
 app.config['MAIL_PASSWORD'] = 'bdb59762a3c0e2'
 
 mail = Mail(app)
+csrf.init_app(app)
+
 from app import views
