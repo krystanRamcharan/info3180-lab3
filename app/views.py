101f14ff2052a99a7dfac30673b11a4616b2c614
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-12 10:32:50 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -18,6 +18,10 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 @app.route('/about/')
 def about():
