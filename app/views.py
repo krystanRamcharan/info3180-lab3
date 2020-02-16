60042f1981d4651a15940a85378a249d00cbdea3
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-16 11:06:09 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -25,14 +25,7 @@ def home():
 @app.route('/contact', methods=('GET', 'POST'))
 def contact():
     form = ContactForm()
-      if form.validate_on_submit():
-        msg=Message(request.form['subject'], sender = request.form['name'],request.form['email'],
-                    recepients=[request.form['email']]) 
-                    msg.body='This is the body'
-                    mail.send(msg)
-         flash('Message sent successfully')
-      return redirect(url_for('/'))
-   return render_template('contact.html',form=form)
+    return render_template('contact.html',form=form)
     
 
 @app.route('/about/')
