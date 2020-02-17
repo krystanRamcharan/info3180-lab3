076ad861a5f7521bdf6e931e7553cfafbc59cdbe
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-17 12:05:33 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import Form, StringField, TextAreaField,validators
 
 
-class ContactForm(Form):
+class ContactForm(FlaskForm):
 name=StringField('Name',[validators.Length(min=5, max=10)])
 email=StringField('Email Address',[validators.Length(min=6,max=20)])
 subject=StringField('Subject',[validators.Length(min=5, max=10)])
